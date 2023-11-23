dd799fdefc5892d12c3e24d78f841730bb3ff138
Johanna Portimo <71264870+jayportimo@users.noreply.github.com>
2023-11-23 22:10:58 +0200

    Prepare for connection between fe and be

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,33 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/Login.css";
 import Header from "./Header.js";
 
-const Login = () => (
-  <div>
-    <Header />
-    <div className="login-container">
-      <div className="top-container">
-        <h2 className="font-bold">Login:</h2>
-        <Link to="/register">
-          <button className="create-account">Create account?</button>
-        </Link>
-      </div>
-      <form className="form-styles">
-        <div className="input-styles">
-          <label>Username: </label>
-          <input type="text" className="input-field" />
-          <div className="input-styles">
-            <label>Password: </label>
-            <input type="password" className="input-field" />
-          </div>
-          <Link to="/packetstatus">
-            <button className="log-in-button">Login</button>
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginError, setLoginError] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // console.logging for now as backend is not connected :)
+    console.log({
+      username,
+      password,
+    });
+
+    // TODO: create backend validation functionality
+    const isLoginSuccessful = true;
+
+    if (isLoginSuccessful) {
+      setLoginError(null);
+
+      // TODO: Redirect to users own pakcetstatus page when successful
+      console.log("Login successful! Redirecting...");
+    } else {
+      setLoginError("Invalid username or password");
+    }
+  };
+
+  return (
+    <div>
+      <Header />
+      <div className="login-container">
+        <div className="top-container">
+          <h2 className="font-bold">Login:</h2>
+          <Link to="/register">
+            <button className="create-account">Create account?</button>
           </Link>
         </div>
-      </form>
+        <form className="form-styles" onSubmit={handleSubmit}>
+          <div className="input-styles">
+            <label>Username: </label>
+            <input
+              type="text"
+              className="input-field"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="input-styles">
+            <label>Password: </label>
+            <input
+              type="password"
+              className="input-field"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          {loginError && <p className="error-message">{loginError}</p>}
+          <Link to="/packetstatus">
+            <button type="submit" className="log-in-button">
+              Login
+            </button>
+          </Link>
+        </form>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Login;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,51 +1,115 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/Register.css";
 import Header from "./Header.js";
 
-const Register = () => (
-  <div>
-    <Header />
-    <div className="register-container">
-      <div className="top-container">
-        <h2 className="font-bold">Register new account:</h2>
-        <Link to="/">
-          <button className="login-instead">Login instead?</button>
-        </Link>
+const Register = () => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [location, setLocation] = useState("location1");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [passwordError, setPasswordError] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      setPasswordError("Passwords do not match");
+      return;
+    }
+
+    setPasswordError(null);
+
+    // console.logging for now as backend is not connected :)
+    console.log({
+      email,
+      username,
+      location,
+      password,
+      confirmPassword,
+    });
+
+    // TODO : here the functionality to send reg. data to backend
+
+    setEmail("");
+    setUsername("");
+    setLocation("");
+    setPassword("");
+    setConfirmPassword("");
+  };
+
+  return (
+    <div>
+      <Header />
+      <div className="register-container">
+        <div className="top-container">
+          <h2 className="font-bold">Register new account:</h2>
+          <Link to="/">
+            <button className="login-instead">Login instead?</button>
+          </Link>
+        </div>
+        <form className="form-styles" onSubmit={handleSubmit}>
+          <div className="input-styles">
+            <label>Email: </label>
+            <input
+              type="text"
+              className="input-field"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="input-styles">
+            <label>Username: </label>
+            <input
+              type="text"
+              className="input-field"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="input-styles">
+            <label>Locations: </label>
+            <select
+              className="input-field"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+            >
+              <option value="location1">Location 1</option>
+              <option value="location2">Location 2</option>
+              <option value="location3">Location 3</option>
+              <option value="location4">Location 4</option>
+              <option value="location5">Location 5</option>
+            </select>
+          </div>
+          <div className="input-styles">
+            <label>Password: </label>
+            <input
+              type="password"
+              className="input-field"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="input-styles">
+            <label>Confirm password: </label>
+            <input
+              type="password"
+              className="input-field"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </div>
+          {passwordError && <p className="error-message">{passwordError}</p>}
+          <Link to="/packetstatus">
+            <button type="submit" className="register-button">
+              Signup
+            </button>
+          </Link>
+        </form>
       </div>
-      <form className="form-styles">
-        <div className="input-styles">
-          <label>Email: </label>
-          <input type="text" className="input-field" />
-        </div>
-        <div className="input-styles">
-          <label>Username: </label>
-          <input type="text" className="input-field" />
-        </div>
-        <div className="input-styles">
-          <label>Locations: </label>
-          <select className="input-field">
-            <option value="location1">Location 1</option>
-            <option value="location2">Location 2</option>
-            <option value="location3">Location 3</option>
-            <option value="location4">Location 4</option>
-            <option value="location5">Location 5</option>
-          </select>
-        </div>
-        <div className="input-styles">
-          <label>Password: </label>
-          <input type="password" className="input-field" />
-        </div>
-        <div className="input-styles">
-          <label>Confirm password: </label>
-          <input type="password" className="input-field" />
-        </div>
-        <Link to="/packetstatus">
-          <button className="register-button">Signup</button>
-        </Link>
-      </form>
     </div>
-  </div>
-);
+  );
+};
 
 export default Register;
