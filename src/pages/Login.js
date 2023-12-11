46e1a6f88337c1638dbff6332eb45a3365bba8a3
Johanna Portimo <71264870+jayportimo@users.noreply.github.com>
2023-12-11 14:38:24 +0200

    localstorage issues

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { useNavigate } from 'react-router-dom';
+import { Link, Navigate } from "react-router-dom";
 import "../styles/Login.css";
 import Header from "./Header.js";
 import { loginUser } from "../fetch/loginFetch.js";
@@ -9,21 +8,30 @@ const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [loginError, setLoginError] = useState("");
-  const navigate = useNavigate();
+  const [redirectToPacketStatus, setRedirectToPacketStatus] = useState(false);
 
   const handleLogin = async (event) => {
     event.preventDefault();
 
     const loginData = {
       username,
-      password
+      password,
     };
-    console.log('loginData:', loginData)
-    
+    console.log("loginData:", loginData);
+
     try {
       const data = await loginUser(loginData);
+      console.log("Login API Response:", data);
+
       if (data.success) {
-        navigate('/packetstatus');
+        if (data.user) {
+          localStorage.setItem("authToken", data.authToken);
+          localStorage.setItem("userEmail", data.user.email);
+          localStorage.setItem("userData", JSON.stringify(data.user));
+          setRedirectToPacketStatus(true);
+        } else {
+          console.error("User is null in the API response");
+        }
       } else {
         setLoginError(data.message);
       }
@@ -31,6 +39,10 @@ const Login = () => {
       setLoginError(`Login error: ${error.message}`);
     }
   };
+
+  if (redirectToPacketStatus) {
+    return <Navigate to="/packetstatus" />;
+  }
 
   return (
     <div>
@@ -40,19 +52,33 @@ const Login = () => {
           <h2 className="font-bold">Login:</h2>
           <Link to="/register">
             <button className="create-account">Create account?</button>
-        </Link>
+          </Link>
         </div>
         <form className="form-styles" onSubmit={handleLogin}>
           <div className="input-styles">
             <label>Username: </label>
-            <input type="text" className="input-field" value={username} onChange={(e) => setUsername(e.target.value)} required/>
+            <input
+              type="text"
+              className="input-field"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
           </div>
           <div className="input-styles">
             <label>Password: </label>
-            <input type="password" className="input-field" value={password} onChange={(e) => setPassword(e.target.value)} required/>
+            <input
+              type="password"
+              className="input-field"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
           </div>
           {loginError && <p className="error-message">{loginError}</p>}
-          <button type="submit" className="log-in-button">Login</button>
+          <button type="submit" className="log-in-button">
+            Login
+          </button>
         </form>
       </div>
     </div>
--- src/pages/PacketStatus.js
+++ src/pages/PacketStatus.js
@@ -6,42 +6,40 @@ import { Link } from "react-router-dom";
 
 export default function Parcels() {
   const [userParcels, setUserParcels] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchUserParcels = async () => {
       try {
-        const authToken = document.cookie
-          .split("; ")
-          .find((row) => row.startsWith("authToken="))
-          .split("=")[1];
-        const userEmail = decodeToken(authToken).email;
+        debugger;
+        const authToken = localStorage.getItem("authToken");
+        const userEmail = localStorage.getItem("userEmail");
 
-        const response = await axios.get(`/parcels?sender_email=${userEmail}`, {
-          headers: {
-            Authorization: `Bearer ${authToken}`,
-          },
-        });
+        if (authToken && userEmail) {
+          const response = await axios.get(
+            `/parcels?sender_email=${userEmail}`,
+            {
+              headers: {
+                Authorization: `Bearer ${authToken}`,
+              },
+            }
+          );
 
-        setUserParcels(response.data);
+          setUserParcels(response.data);
+        } else {
+          setError("User email not found in localStorage");
+        }
       } catch (error) {
-        console.error("Error fetching parcels for user:", error);
-      }
-    };
-
-    const decodeToken = (token) => {
-      try {
-        const base64Url = token.split(".")[1];
-        const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-        const decoded = JSON.parse(atob(base64));
-        return decoded;
-      } catch (error) {
-        console.error("Error decoding token:", error);
-        return {};
+        setError(`Error fetching parcels for user: ${error.message}`);
+      } finally {
+        setLoading(false);
       }
     };
 
     fetchUserParcels();
   }, []);
+
   const transformStatus = (lockerStatus) => {
     if (lockerStatus === "ready for customer pickup") {
       return "ready for pickup";
@@ -63,6 +61,14 @@ export default function Parcels() {
         return "transparent-ball";
     }
   };
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   return (
     <div>
